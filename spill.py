0ebc180f6f4889c0a77e541dca907fc36afdb997
HenrikS-A <hstromandersen@gmail.com>
2023-11-15 21:08:49 +0100

    Begynt på spillet. (laget enkle klasser, tegner figurene og flytter bakgrunnen)

--- spill.py
+++ spill.py
@@ -1,25 +1,70 @@
 import pygame
 
 # Klasser
-class Matbit:
-    def __init__(self):
-        self.bilde = pygame.image.load("bilder/stemme.png").convert_alpha()
-        self.ramme
+class Figur:
+    def __init__(self, bildeurl: str, storrelse: float):
+        self.originalt_bilde = pygame.image.load(bildeurl).convert_alpha()
+        self.original_storrelse = self.originalt_bilde.get_size()
+        self.bilde = pygame.transform.scale(self.originalt_bilde, (self.original_storrelse[0] * storrelse, self.original_storrelse[1] * storrelse))
+        self.ramme = self.bilde.get_rect()
+
+    def tegn(self, vindu, x, y):
+        vindu.blit(self.bilde, (x, y))
+
+
+
+class Bakgrunn(Figur):
+    def __init__(self, bildeurl: str, storrelse: float):
+        super().__init__(bildeurl, storrelse)
+
+    def tegn(self, vindu, x, y):
+        super().tegn(vindu, x, y)
+
+
+    def flytt_bakgrunn():
+        for i in range(2):
+            bakgrunn.tegn(vindu, i * bakgrunn.ramme.width + scroll, 0) # Tegner den første bg i (0,0) og den andre bg ved siden av den første. De flytter seg med scroll
+
+
+
+class Bil(Figur):
+    def __init__(self, bildeurl: str, storrelse: float):
+        super().__init__(bildeurl, storrelse)
+        self.fart = 0
+
+    def tegn(self, vindu, x, y):
+        super().tegn(vindu, x, y)
+
+    def flytt_bil():
+        pass
+
+
 
 
 
 # 1. Oppsett
 pygame.init()
 
-BREDDE = 1280
-HOYDE = 720
+SKJERM_BREDDE = 1280
+SKJERM_HOYDE = 720
 FPS = 60
 
-vindu = pygame.display.set_mode((BREDDE, HOYDE))
+vindu = pygame.display.set_mode((SKJERM_BREDDE, SKJERM_HOYDE))
 klokke = pygame.time.Clock()
-
-pygame.display.set_caption("Parti Agario") # Navn på vinduet
+pygame.display.set_caption("Drag Race") 
 font = pygame.font.SysFont("Open Sans", 24) # Skrifttype
+
+
+
+bakgrunn = Bakgrunn("bilder/bakgrunn.jpg", 1.5) 
+
+spillerbil = Bil("bilder/spillerbil.png", 1.1)
+pcbil = Bil("bilder/pcbil.png", 0.35)
+
+
+
+scroll = 0
+
 
 while True:
     # 2. Håndter input
@@ -30,19 +75,21 @@ while True:
     
     # 3. Oppdater spill
     # 4. Tegn
+    vindu.fill("gray") # Fyller vinduet med hvit bakgrunn, for hver gang loopen kjører.
 
-    vindu.fill("white") # Fyller vinduet med hvit bakgrunn, for hver gang loopen kjører.
+    
+    Bakgrunn.flytt_bakgrunn()
+    scroll -= 4.321
 
-
-    pygame.draw.circle(vindu, "green", (100, 100), 40) # Tegner en sirkel
-    pygame.draw.rect(vindu, "red", (300, 300, 200, 100)) # Tegner et rektangel
-
-
-    tekst_hallo = font.render("Hallo på deg!", True, "black") # Oppretter tekst
-    vindu.blit(tekst_hallo, (BREDDE//2 - tekst_hallo.get_width()//2, HOYDE * 10/100)) # Tegner teksten i midten av x-posisjon og 10% ned fra toppen
+    if abs(scroll) > bakgrunn.ramme.width: # absoluttverdien av scoll, siden den blir mer og mer negativ. Gir mer mening sånn
+        scroll = 0
 
 
 
+    pcbil.tegn(vindu, 100, 230)
+    spillerbil.tegn(vindu, 100, 345)
 
-    pygame.display.flip() # Oppdaterer skjermen
+
+
+    pygame.display.flip()
     klokke.tick(FPS)
